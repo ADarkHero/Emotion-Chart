2d462550a4bb876ee3d69276382712b49e0640de
ADarkHero <maik.riedlsperger@gmail.com>
2019-08-06 15:43:12 +0200

    Update chartHelper.php
    Reversed the chart.

--- php/chartHelper.php
+++ php/chartHelper.php
@@ -5,14 +5,21 @@
     $average = "[";
     $paverage = "[";
     $pnumber = 0;
-    for($i = 0; $row = $statement->fetch(); $i++) {
-        $labels = $labels.'"'.$row[1].'", ';
-        $data = $data.'"'.$row[2].'", ';
+    
+    //Writes the data into an array, so I can read it in reverse order later on.
+    while($row = $statement->fetch()){
+	    $newData[] = $row;
+	}
+	
+	for($i = count($newData)-1; $i >= 0; $i--) {
+        $labels = $labels.'"'.$newData[$i][1].'", ';
+        $data = $data.'"'.$newData[$i][2].'", ';
         $average = $average.'"3.5", ';
-        $pnumber = $pnumber + $row[2];
-        $pcalculated = $pnumber / ($i+1);
+        $pnumber = $pnumber + $newData[$i][2];
+        $pcalculated = $pnumber / (count($newData)-$i);
         $paverage = $paverage.'"'.$pcalculated.'", ';
     }
+    echo $i;
     $data = substr($data, 0, -2);
     $labels = substr($labels, 0, -2);
     $average = substr($average, 0, -2);
